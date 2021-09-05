2a0b62f89a175542359afaa640e0e12e9dd6a23a
Alexis Okamura <alexiskokamura@gmail.com>
2021-09-05 16:49:46 -0700

    Base badge color on movie types

--- src/components/SearchResults/index.js
+++ src/components/SearchResults/index.js
@@ -1,8 +1,38 @@
 import React from 'react';
 import './SearchResults.css';
 
+const MovieTypes = {
+    episode: 'episode',
+    game: 'game',
+    movie: 'movie',
+    series: 'series',
+};
+
+const BadgeColors = {
+    blue: 'cornflowerblue',
+    green: 'darkseagreen',
+    orange: 'darkorange',
+    red: 'indianred',
+    yellow: 'gold'
+}
+
 const SearchResults = (props) => {
     const { results = [], setMovie } = props;
+
+    const getBadgeColor = (type) => {
+        switch(type) {
+            case MovieTypes.episode:
+                return BadgeColors.red;
+            case MovieTypes.game:
+                return BadgeColors.green;
+            case MovieTypes.movie:
+                return BadgeColors.blue;
+            case MovieTypes.series:
+                return BadgeColors.yellow;
+            default:
+                return BadgeColors.orange;
+        }
+    };
 
     const onListClick = (result) => {
         setMovie(result);
@@ -14,7 +44,7 @@ const SearchResults = (props) => {
         <ul className="search-results">
             {results.map((result, index) => (
                 <li className={`result ${index === 0 && 'result--no-margin'}`} onClick={() => onListClick(result)}>
-                    <span className="result__badge">{result['Type']}</span>
+                    <span className="result__badge" style={{ background: getBadgeColor(result['Type'])}}>{result['Type']}</span>
                     <span className="result__title">{result['Title']}</span>
                     <span className="result__year">{result['Year']}</span>
                 </li>
