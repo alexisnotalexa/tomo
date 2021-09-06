95bc685bf04ef7e47ce9468b7924d52957430f1a
Alexis Okamura <alexiskokamura@gmail.com>
2021-09-06 14:51:22 -0700

    Add handler for broken movie poster URLs

--- src/components/Movie/index.js
+++ src/components/Movie/index.js
@@ -1,13 +1,25 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './Movie.css';
 
 const Movie = (props) => {
     const { movie } = props;
+    const [isImageBroken, setIsImageBroken] = useState(false);
+
+    useEffect(() => {
+        setIsImageBroken(false);
+    }, [movie]);
 
     return (
         <div className="movie">
-            <img className="movie__img" src={movie['Poster']} alt="temp-img" />
-            <span className="movie__title">{movie['Title']}</span>
+            {isImageBroken 
+                ? <div className="movie__img--error"><span>No image available.</span></div> 
+                : <img 
+                    className="movie__img" src={movie['Poster']} 
+                    alt={`${movie['Title']} Poster`} 
+                    onError={() => setIsImageBroken(true)} 
+                    onLoad={() => setIsImageBroken(false)} 
+            />}
+            <h3 className="movie__title">{movie['Title']}</h3>
         </div>
     );
 };
